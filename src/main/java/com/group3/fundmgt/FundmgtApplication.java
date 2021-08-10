7e4e366a0ccb5b68ca4281acf8b6f190ba5d14f0
Hz <36901033+Zheng7426@users.noreply.github.com>
2021-08-10 15:01:04 +0000

    seed db within entrypoint

--- src/main/java/com/group3/fundmgt/FundmgtApplication.java
+++ src/main/java/com/group3/fundmgt/FundmgtApplication.java
@@ -1,7 +1,13 @@
 package com.group3.fundmgt;
 
+import com.group3.fundmgt.manager.Manager;
+import com.group3.fundmgt.manager.ManagerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class FundmgtApplication {
@@ -10,4 +16,14 @@ public class FundmgtApplication {
         SpringApplication.run(FundmgtApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(ManagerRepository managerRepository) {
+        return args -> {
+            List<Manager> managers = List.of(
+                    new Manager(1, "Chris", "Gardner"),
+                    new Manager(2, "Frank", "Abagnale")
+            );
+            managerRepository.saveAll(managers);
+        };
+    }
 }
