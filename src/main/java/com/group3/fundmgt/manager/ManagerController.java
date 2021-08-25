7ec62e8fad9486738c0f9aa0ecb668e1a3455e36
Ke Li <ytyuyunting@gmail.com>
2021-08-25 06:56:04 +0000

    add cross origin

--- src/main/java/com/group3/fundmgt/manager/ManagerController.java
+++ src/main/java/com/group3/fundmgt/manager/ManagerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/managers")
 public class ManagerController {
     private final ManagerService managerService;
