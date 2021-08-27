66b27206d3a6bbec512c5681177a72fdb9a2e562
poohdini1203 <939015806@qq.com>
2021-08-27 09:41:38 +0800

    fund 分类接口,manager 分类接口

--- src/main/java/com/group3/fundmgt/manager/ManagerController.java
+++ src/main/java/com/group3/fundmgt/manager/ManagerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @RequestMapping("/api/v1/managers")
 public class ManagerController {
 
